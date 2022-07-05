743d6e2b972c4ff4550415bd11939b4510a7b7f6
Viktor Avelino <viktor.avelino@gmail.com>
2022-07-04 20:29:41 -0400

    deploy

---
+++ _next/static/5TEWK6GsmNtpd10aqFf2g/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/382-18a0907631bba6f0.js","static/chunks/pages/index-13bafe89458c9f74.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/RZedonMbjgB-6901toGsC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/382-ca8a734914ab77cc.js","static/chunks/pages/index-263cc9463aa4c8b8.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
