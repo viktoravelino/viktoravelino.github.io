62b3a0d1bdc01982f97aa714addb927216437b9c
Viktor Avelino <viktor.avelino@gmail.com>
2026-02-09 09:26:47 -0500

    astro setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://viktoravelino.github.io",
+	base: "/portfolio",
+	output: "static",
+	image: {
+		service: {
+			entrypoint: "astro/assets/services/sharp",
+		},
+	},
+});
